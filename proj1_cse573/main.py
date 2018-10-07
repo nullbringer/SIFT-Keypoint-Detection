74b87f97b5d7cafe7e03ded43bdfeadc993744e3
urbangeek <gupta.amlan@gmail.com>
2018-10-06 22:12:21 -0400

    dynamic DoG

--- proj1_cse573/main.py
+++ proj1_cse573/main.py
@@ -24,30 +24,15 @@ def convolve_img(img, kernel,kernel_radius):
 
 	height, width = img.shape
 
-	output_image = np.zeros(img.shape, np.uint8)
+	output_image = np.zeros(img.shape, np.int)
 
 	# ignoring edge pixels for now.
 	# add padding zero
 
 	for i in range(kernel_radius, height-kernel_radius):
 		for j in range(kernel_radius, width-kernel_radius):
-			
-			# output_image[i][j] = 	img[i-1][j-1] * kernel[0][0] +	\
-			# 						img[i][j-1] * kernel[1][0] +	\
-			# 						img[i+1][j-1] * kernel[2][0] + 	\
-			# 						img[i-1][j] * kernel[0][1] + 	\
-			# 						img[i][j] * kernel[1][1] + 		\
-			# 						img[i+1][j] * kernel[2][1] + 	\
-			# 						img[i-1][j+1] * kernel[0][2] + 	\
-			# 						img[i][j+1] * kernel[1][2] + 	\
-			# 						img[i+1][j+1] * kernel[2][2]
-
-
-
-
 
 			# elementwise multiplication sum
-
 			
 			loop_end = (kernel_radius*2)+1
 
@@ -56,12 +41,7 @@ def convolve_img(img, kernel,kernel_radius):
 				for y in range(0,loop_end):
 					sum += kernel[x][y] * img[i-kernel_radius+x][j-kernel_radius+y]
 
-
-			
-
-
 			output_image[i][j] = sum
-
 
 
 	return output_image
@@ -74,10 +54,28 @@ def edge_detection_x(img):
 							[-1, 0, 1], 
 							[-2, 0, 2], 
 							[-1, 0 , 1] 
-						], np.int8)  
+						], np.float32)  
 
 	edge_x_img = convolve_img(img,x_kernel,1)
-	print_image(edge_x_img,'edge_x_img')
+
+	# print_image(edge_x_img,'x edge')
+	h,w = edge_x_img.shape
+
+	max_val = 0
+	for i in range(0,h):
+		for j in range(1,w):
+			edge_x_img[i][j] = abs(edge_x_img[i][j])
+			max_val = max(max_val,edge_x_img[i][j])
+
+	pos_edge_x = np.zeros(img.shape, np.float32)
+
+	for i in range(0,h):
+		for j in range(1,w):
+			pos_edge_x[i][j] = edge_x_img[i][j]/max_val
+
+	
+	print_image(pos_edge_x,'x_edge_detection_normalized')
+
 
 
 
@@ -87,11 +85,28 @@ def edge_detection_y(img):
 							[-1, -2, -1], 
 							[0, 0, 0], 
 							[1, 2 , 1] 
-						], np.int8)  
+						], np.float32)  
 
 
 	edge_y_img = convolve_img(img,y_kernel,1)
-	print_image(edge_y_img,'edge_y_img')
+	# print_image(edge_y_img,'edge_y_img')
+
+	h,w = edge_y_img.shape
+
+	max_val = 0
+	for i in range(0,h):
+		for j in range(1,w):
+			edge_y_img[i][j] = abs(edge_y_img[i][j])
+			max_val = max(max_val,edge_y_img[i][j])
+
+	pos_edge_y = np.zeros(img.shape, np.float32)
+
+	for i in range(0,h):
+		for j in range(1,w):
+			pos_edge_y[i][j] = edge_y_img[i][j]/max_val
+
+	
+	print_image(pos_edge_y,'y_edge_detectioin_normalized')
 
 
 def gaussian(x, mu, sigma):
@@ -119,7 +134,7 @@ def resize_image_to_half(img):
 
 	height, width = img.shape
 
-	output_image = np.zeros((int(height/2), int(width/2)), np.uint8)
+	output_image = np.zeros((int(height/2), int(width/2)), np.int)
 
 
 	i_op = 0
@@ -139,7 +154,6 @@ def resize_image_to_half(img):
 		i_op+=1
 
 	
-	#print_image(output_image,'output_image')
 	return output_image
 
 
@@ -147,12 +161,10 @@ def resize_image_to_half(img):
 def generate_gaussian_blur_for_an_image(img, octav_id, sigma_row):
 
 
-	for i in range(len(sigma_row)):
-
-		
+	for i in range(len(sigma_row)):		
 
 		gussian_blurred_img = convolve_img(img, get_gaussian_kernel(sigma_row[i]), 3)
-		#gussian_blurred_img  = cv2.GaussianBlur(img, (7,7), sigma_row[i],0)
+		
 		write_image(gussian_blurred_img,'gb_img_'+ octav_id +'_'+str(i))
 
 
@@ -186,33 +198,49 @@ def generate_octavs(image_1,sigma_table):
 	write_image(image_4,'octav_4_original')
 	generate_gaussian_blur_for_an_image(image_4,'octav_4', sigma_table[3])
 
-	# print(sigma_table[3])
 
 
-def compute_DoG():
 
-	for i in range(0,4):
-		for j in range(1,5):
+def compute_DoG(list):
+
+	
+
+	for j in range(1,5):
+		for i in range(0,4):
+		
 
 			img_lower_blur = cv2.imread("gb_img_octav_" + str(j) + "_" + str(i) + ".png", 0)
 			img_higher_blur = cv2.imread("gb_img_octav_" + str(j) + "_" + str(i+1) + ".png", 0)
+
+		
 			
-			write_image(img_higher_blur-img_lower_blur,'dog_octav_'+ str(j)+'_'+ str(i))
+
+
+			height, width = img_lower_blur.shape
+			difference = np.zeros(img_lower_blur.shape, dtype=int)
+
+
+			for h in range(0,height):
+				for w in range(0, width):
+					difference[h][w] = int(img_higher_blur[h][w]) - int(img_lower_blur[h][w])
 
 
 
-def find_maxima_minima(octav_num,layer):
+			
+			write_image(difference,'dog_octav_'+ str(j)+'_'+ str(i))
+			
+			list.append(difference)
 
-	output_image = cv2.imread("octav_" + str(octav_num) + "_original.png", 0)
-
-
-	dog_top = cv2.imread("dog_octav_" + str(octav_num) + "_" + str(layer-1) + ".png", 0)
-	dog_middle = cv2.imread("dog_octav_" + str(octav_num) + "_"+ str(layer) + ".png", 0)
-	dog_bottom = cv2.imread("dog_octav_" + str(octav_num) + "_" + str(layer+1) + ".png", 0)
+	return list
 
 
+
+def find_marked_maxima_minima(dog_top, dog_middle, dog_bottom, scale_multiplier, original_img):
 
 	height, width = dog_middle.shape
+
+	print(np.max(dog_middle))
+	print(np.min(dog_middle))
 
 
 	# traversing image
@@ -223,8 +251,9 @@ def find_maxima_minima(octav_num,layer):
 		for w in range(1, width-1):
 
 			#threshold
-			if dog_middle[h][w]<200:
+			if dog_middle[h][w]<2:
 				continue
+
 
 
 			# traversing and comparing 26 neighbours
@@ -240,40 +269,114 @@ def find_maxima_minima(octav_num,layer):
 					break
 
 			if is_maxima:
-				output_image[h][w] = 255
-			else:
+				original_img[h*scale_multiplier][w*scale_multiplier] = 255
+			# else:
 
-				is_minima = False
+			# 	is_minima = False
 
-				for i in range(h-1,h+2):
-					for j in range(w-1,w+2):
-						if (dog_middle[h][w] > dog_middle[i][j]) or (dog_middle[h][w] > dog_top[i][j]) or (dog_middle[h][w] > dog_bottom[i][j]):
-							is_minima = False
-							break
+			# 	for i in range(h-1,h+2):
+			# 		for j in range(w-1,w+2):
+			# 			if (dog_middle[h][w] > dog_middle[i][j]) or (dog_middle[h][w] > dog_top[i][j]) or (dog_middle[h][w] > dog_bottom[i][j]):
+			# 				is_minima = False
+			# 				break
 
-					if not is_minima:
-						break
-				if is_minima:
-					output_image[h][w] = 255
+			# 		if not is_minima:
+			# 			break
+			# 	if is_minima:
+			# 		original_img[h*scale_multiplier][w*scale_multiplier] = 255
 
-	print_image(output_image,'keypoints'+str(layer)+str(h)+str(w))
+	#print_image(original_img,'keypoints'+str(layer)+str(h)+str(w))
+	return original_img
 
-def find_keypoints():
+def find_keypoints(original_img, list_of_dog):
+
+
+	# scale_multiplier = 1
+
+	# #traversing 4 octavs
+	# for octav_num in range(1,5):
+
+	# 	# traversing 2 middle layers
+	# 	for layer in range(1,3):
+			
+	# 		original_img = find_marked_maxima_minima(list_of_dog[], scale_multiplier, original_img)
+
+	# 	scale_multiplier = scale_multiplier*2
+
+
+	find_marked_maxima_minima(list_of_dog[0],list_of_dog[1],list_of_dog[2], 1, original_img)
+	find_marked_maxima_minima(list_of_dog[1],list_of_dog[2],list_of_dog[3], 1, original_img)
+
+	find_marked_maxima_minima(list_of_dog[4],list_of_dog[5],list_of_dog[6], 2, original_img)
+	find_marked_maxima_minima(list_of_dog[5],list_of_dog[6],list_of_dog[7], 2, original_img)
+
+	find_marked_maxima_minima(list_of_dog[8],list_of_dog[9],list_of_dog[10], 4, original_img)
+	find_marked_maxima_minima(list_of_dog[9],list_of_dog[10],list_of_dog[11], 4, original_img)
+
+	find_marked_maxima_minima(list_of_dog[12],list_of_dog[13],list_of_dog[14], 8, original_img)
+	find_marked_maxima_minima(list_of_dog[13],list_of_dog[14],list_of_dog[15], 8, original_img)
 
 
 
-	#traversing 4 octavs
-	for octav_num in range(1,5):
+
+	write_image(original_img,'keypoints')
+
+
+
+def find_cursor():
+
+
+	for img_no in range(1,16):
+
+		original_image = cv2.imread('task3/pos_' + str(img_no) + '.jpg')
+
+		img_source = cv2.imread('task3/pos_' + str(img_no) + '.jpg',0)
 		
+		#template = cv2.imread('task3/template.png',0)
+		template = cv2.imread('task3/temp.jpg',0)
 
-		# traversing 2 middle layers
-		for layer in range(1,3):
-			
-			find_maxima_minima(octav_num,layer)
-			
+		
+		laplacian = cv2.Laplacian(cv2.GaussianBlur(img_source, (3,3),0),cv2.CV_8U)
+		img2 = laplacian.copy()
+		template = cv2.Laplacian(template,cv2.CV_8U)
+
+		# print_image(laplacian,"sdf")
+		# print_image(template,"sdfsdfsdf")
 
 
-			
+
+
+
+		w, h = template.shape[::-1]
+
+		#print_image(img_source,"sdfs")
+
+		# All the 6 methods for comparison in a list
+		methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+		            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+		for meth in methods:
+
+			oi = original_image.copy()
+			img = img2.copy()
+			method = eval(meth)
+
+			# Apply template Matching
+			res = cv2.matchTemplate(img,template,method)
+
+			min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+			# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+			if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+			    top_left = min_loc
+			else:
+			    top_left = max_loc
+			bottom_right = (top_left[0] + w, top_left[1] + h)
+
+			cv2.rectangle(oi,top_left, bottom_right, 255, 2)
+			#print_image(img,"hello")
+			write_image(oi, 'task3/output/' + str(img_no) + meth)
+
 
 
 
@@ -286,10 +389,9 @@ def main():
 	# edge_detection_y(task_1_img)
 
 
-	#task 2
+	# #task 2
 
 	task_2_img = cv2.imread("task2.jpg", 0)
-	# task_2_img = cv2.imread("testcat.jpg", 0)
 
 	sigma_table = np.array([				
 							[1/sqrt(2), 1, sqrt(2), 2, 2*sqrt(2)], 
@@ -301,9 +403,24 @@ def main():
 
 	
 
-	# generate_octavs(task_2_img, sigma_table);
-	# compute_DoG()
-	find_keypoints()
+	#generate_octavs(task_2_img, sigma_table);
+
+	list = []
+	compute_DoG(list)
+	find_keypoints(task_2_img, list)
+
+
+	# #find_cursor()
+
+
+
+
+
+
+
+
+
+
 
 
 
